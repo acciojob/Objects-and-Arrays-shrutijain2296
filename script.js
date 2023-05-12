c2ed4bccfaff5565b318e6a3b54df57d231dc6bf
shrutijain2296 <128174123+shrutijain2296@users.noreply.github.com>
2023-05-12 17:04:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const person = {
 };
 
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
+const team = players;
+const team1 = Array.from(Players);
+const cap1 = Object.assign({}, person);
 
 window.players = players;
 window.person = person;
